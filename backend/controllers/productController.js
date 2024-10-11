0c8619c21b8cc730eaf22edddfebfc44f558e9bf
Gideon1107 <ayenigideon1107@gmail.com>
2024-10-11 22:02:44 +0100

    Backend: Login & Register API Endpoint done

---
+++ backend/controllers/productController.js
@@ -0,0 +1,27 @@
+
+
+
+// Function for adding product
+const addProduct = async (req,res) => {
+    
+}
+
+
+// Function for List product
+const listProduct = async (req,res) => {
+
+}
+
+
+// Function for removing product
+const removeProduct = async (req,res) => {
+
+}
+
+
+// Function for single product info
+const singleProduct = async (req,res) => {
+
+}
+
+export {addProduct, listProduct, removeProduct, singleProduct};
